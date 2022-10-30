7f52066b6811b24a64c525eae6c441a03fb40766
Carlos Pantelides <cpantel@github.com>
2022-10-30 11:37:33 -0300

    alternative java

---
+++ java/alt_java.sh
@@ -0,0 +1,9 @@
+# locate java | grep "/java$"
+# /home/user/Xilinx/Vivado_HLS/2015.2/tps/lnx64/jre/bin/java
+# /home/carlos/bin/ums-11.4.0/jre17/bin/java
+# ...
+
+export JAVA_HOME=/home/user/Xilinx/Vivado_HLS/2015.2/tps/lnx64/jre
+export PATH=$JAVA_HOME/bin:$PATH
+
+java ...
